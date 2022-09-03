0b84a6064b7d1dd212b6f41eed8e97577c027912
Lawrence LCI <lcheunglci@gmail.com>
2022-09-03 16:01:00 -0700

    Create migration for adding the Gigs table

--- GigHub/Data/ApplicationDbContext.cs
+++ GigHub/Data/ApplicationDbContext.cs
@@ -1,10 +1,15 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using GigHub.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace GigHub.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Gig> Gigs { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
---
+++ GigHub/Models/Gig.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace GigHub.Models
+{
+    public class Gig
+    {
+        public int Id { get; set; }
+        public IdentityUser Artist { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Venue { get; set; }
+
+        public Genre Genre { get; set; }
+    }
+
+}
