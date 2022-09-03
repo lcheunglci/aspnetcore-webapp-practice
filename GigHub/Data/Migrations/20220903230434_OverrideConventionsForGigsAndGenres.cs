09dfeba46105013fd1c946a7a68549f5c8014b36
Lawrence LCI <lcheunglci@gmail.com>
2022-09-03 16:09:09 -0700

    Override Conventions for Gigs and Genres with Data Annotations

---
+++ GigHub/Data/Migrations/20220903230434_OverrideConventionsForGigsAndGenres.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GigHub.Data.Migrations
+{
+    public partial class OverrideConventionsForGigsAndGenres : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Gigs_AspNetUsers_ArtistId",
+                table: "Gigs");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Venue",
+                table: "Gigs",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ArtistId",
+                table: "Gigs",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Genres",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Gigs_AspNetUsers_ArtistId",
+                table: "Gigs",
+                column: "ArtistId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Gigs_AspNetUsers_ArtistId",
+                table: "Gigs");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Venue",
+                table: "Gigs",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ArtistId",
+                table: "Gigs",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Genres",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Gigs_AspNetUsers_ArtistId",
+                table: "Gigs",
+                column: "ArtistId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
--- GigHub/Models/Gig.cs
+++ GigHub/Models/Gig.cs
@@ -1,14 +1,21 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace GigHub.Models
 {
     public class Gig
     {
         public int Id { get; set; }
+
+        [Required]
         public IdentityUser Artist { get; set; }
+
         public DateTime DateTime { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Venue { get; set; }
 
+        [Required]
         public Genre Genre { get; set; }
     }
 
