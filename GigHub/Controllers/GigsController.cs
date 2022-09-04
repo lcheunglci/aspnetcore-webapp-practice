ae4d8e938010021840e952c28252f1ad370429a2
Lawrence LCI <lcheunglci@gmail.com>
2022-09-04 00:19:45 -0700

    Add a basic view

---
+++ GigHub/Controllers/GigsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class GigsController : Controller
+    {
+        // GET: Gigs
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+    }
+}
