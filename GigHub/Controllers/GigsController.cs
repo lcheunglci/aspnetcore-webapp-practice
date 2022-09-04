d9dfebd64666b2ac80e4bf1453692bcdc7c421ae
Lawrence LCI <lcheunglci@gmail.com>
2022-09-04 08:08:30 -0700

    Add form for creating a gig

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -1,13 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GigHub.Data;
+using GigHub.ViewModel;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GigHub.Controllers
 {
     public class GigsController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public GigsController(ApplicationDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+
         // GET: Gigs
         public IActionResult Create()
         {
-            return View();
+            var viewModel = new GigFormViewModel()
+            {
+                Genres = _context.Genres.ToList()
+            };
+            return View(viewModel);
         }
 
     }
