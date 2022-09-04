5967dd79bf64c6e7dd0932922ae966a72869c997
Lawrence LCI <lcheunglci@gmail.com>
2022-09-03 23:48:05 -0700

    Add migration for populating genres tables

---
+++ GigHub/Data/Migrations/20220904063551_PopulateGenresTable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GigHub.Data.Migrations
+{
+    public partial class PopulateGenresTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Genres (Id, Name) VALUES(1, 'Jazz')");
+            migrationBuilder.Sql("INSERT INTO Genres (Id, Name) VALUES(2, 'Blues')");
+            migrationBuilder.Sql("INSERT INTO Genres (Id, Name) VALUES(3, 'Rock')");
+            migrationBuilder.Sql("INSERT INTO Genres (Id, Name) VALUES(4, 'Country')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Genres WHERE Id IN (1, 2, 3, 4)");
+        }
+    }
+}
