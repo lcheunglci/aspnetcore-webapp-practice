0afb813b4ebfd22e996155a682cd7a4bb4d98a77
Lawrence LCI <lcheunglci@gmail.com>
2022-09-07 23:31:45 -0700

    Add api to add attendance

---
+++ GigHub/Controllers/AttendancesController.cs
@@ -0,0 +1,48 @@
+﻿using GigHub.Data;
+using GigHub.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GigHub.Controllers
+{
+    [Route("api/attendances")]
+    [ApiController]
+    [Authorize]
+    public class AttendancesController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+
+        public AttendancesController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public IActionResult Attend([FromBody] int gigId)
+        {
+            var userId = _userManager.GetUserId(User);
+
+
+            if (_context.Attendances.Any(a => a.AttendeeId == userId && a.GigId == gigId))
+            {
+                return BadRequest("The attendance already exists.");
+            }
+
+            var attendance = new Attendance()
+            {
+                GigId = gigId,
+                AttendeeId = userId
+            };
+
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+    }
+}
