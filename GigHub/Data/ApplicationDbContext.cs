48878b396da70ba9874d2160cb324d845da722b0
Lawrence LCI <lcheunglci@gmail.com>
2022-09-07 22:51:09 -0700

    Add Attendance and add migration

--- GigHub/Data/ApplicationDbContext.cs
+++ GigHub/Data/ApplicationDbContext.cs
@@ -8,11 +8,33 @@ namespace GigHub.Data
     {
         public DbSet<Gig> Gigs { get; set; }
         public DbSet<Genre> Genres { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
 
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            //builder.Entity<Attendance>().HasKey(table =>
+            //    new
+            //    {
+            //        table.AttendeeId,
+            //        table.GigId
+            //    });
+
+            builder.Entity<Attendance>()
+                .HasKey(nameof(Attendance.GigId), nameof(Attendance.AttendeeId));
+
+            builder.Entity<Attendance>()
+                .HasOne(a => a.Gig)
+                .WithMany()
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            base.OnModelCreating(builder);
         }
     }
 }
---
+++ GigHub/Models/Attendance.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GigHub.Models
+{
+    public class Attendance
+    {
+        public Gig Gig { get; set; }
+        public ApplicationUser Attendee { get; set; }
+
+        // In EF Core, Composite Keys are generated from the DbContext using the modelBuilder with .HasKey(...)
+        [Key]
+        [Column(Order = 1)]
+        // [Required]
+        public int GigId { get; set; }
+
+        [Key]
+        [Column(Order = 2)]
+        // [Required]
+        public string AttendeeId { get; set; }
+
+    }
+}
