ba163db7bd5617577533cb5f96fb98bbfdc24612
Lawrence LCI <lcheunglci@gmail.com>
2022-09-08 23:09:26 -0700

    Implement the logic for the following api button logic

---
+++ GigHub/Controllers/FollowingsController.cs
@@ -0,0 +1,47 @@
+﻿using GigHub.Data;
+using GigHub.Dtos;
+using GigHub.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GigHub.Controllers
+{
+    [Route("api/followings")]
+    [ApiController]
+    [Authorize]
+    public class FollowingsController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+        private UserManager<ApplicationUser> _userManager;
+
+        public FollowingsController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public IActionResult Follow(FollowingDto dto)
+        {
+            var userId = _userManager.GetUserId(User);
+
+            if (_context.Followings.Any(f => f.FolloweeId == userId && f.FolloweeId == dto.FolloweeId))
+            {
+                return BadRequest("Following already exists.");
+            }
+
+            var following = new Following
+            {
+                FollowerId = userId,
+                FolloweeId = dto.FolloweeId
+            };
+
+            _context.Followings.Add(following);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+    }
+}
