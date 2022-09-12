e349fb4ae4f78ee96557ffd0574f6b1f5e14e4a0
Lawrence LCI <lcheunglci@gmail.com>
2022-09-11 17:23:06 -0700

    Add delete button in the my gigs page

---
+++ GigHub/API/GigsController.cs
@@ -0,0 +1,36 @@
+﻿using GigHub.Data;
+using GigHub.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Web.Http;
+
+namespace GigHub.API
+{
+    [Microsoft.AspNetCore.Mvc.Route("api/gigs")]
+    [ApiController]
+    [Authorize]
+    public class GigsController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+        private UserManager<ApplicationUser> _userManager;
+
+        public GigsController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [System.Web.Http.HttpDelete]
+        public IActionResult Cancel(int id)
+        {
+            var userId = _userManager.GetUserId(User);
+
+            var gig = _context.Gigs.Single(g => g.Id == id && g.ArtistId == userId);
+
+            gig.IsCanceled = true;
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
