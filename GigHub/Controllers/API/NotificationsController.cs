d71dd61c7a66edc7b8b93f62e09d094a4e7fab1f
Lawrence LCI <lcheunglci@gmail.com>
2022-09-14 00:26:02 -0700

    Add the notifications api endpoint

---
+++ GigHub/Controllers/API/NotificationsController.cs
@@ -0,0 +1,37 @@
+﻿using GigHub.Data;
+using GigHub.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GigHub.Controllers.API
+{
+    [Route("api/notifications")]
+    [ApiController]
+    [Authorize]
+    public class NotificationsController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+        private UserManager<ApplicationUser> _userManager;
+
+        public NotificationsController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IEnumerable<Notification> GetNewNotifications()
+        {
+            var userId = _userManager.GetUserId(User);
+            var notifications = _context.UserNotifications
+                .Where(un => un.UserId == userId)
+                .Select(un => un.Notification)
+                .Include(n => n.Gig.Artist)
+                .ToList();
+
+            return notifications;
+        }
+    }
+}
