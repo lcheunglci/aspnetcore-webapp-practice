7a41ab1673182c98d72e3582f8a00647223ffabf
Lawrence LCI <lcheunglci@gmail.com>
2022-10-03 22:44:40 -0700

    Add test using the dbset mock and check get upcoming gigs in the past return empty list

---
+++ GigHub.Tests/Extensions/MockDbSetExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+
+namespace GigHub.Tests.Extensions
+{
+    public static class MockDbSetExtensions
+    {
+        public static void SetSource<T>(this Mock<DbSet<T>> mockSet, IList<T> source) where T : class
+        {
+            var data = source.AsQueryable().AsQueryable();
+
+            mockSet.As<IQueryable<T>>().Setup(m => m.Provider).Returns(data.Provider);
+            mockSet.As<IQueryable<T>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockSet.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator);
+        }
+    }
+}
---
+++ GigHub.Tests/Persistence/Repositories/GigRepositoryTests.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using GigHub.Core.Models;
+using GigHub.Persistence;
+using GigHub.Repositories;
+using GigHub.Tests.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+
+namespace GigHub.Tests.Persistence.Repositories
+{
+
+    public class GigRepositoryTests
+    {
+        private GigRepository _repository;
+        private Mock<DbSet<Gig>> _mockGigs;
+
+        public GigRepositoryTests()
+        {
+            _mockGigs = new Mock<DbSet<Gig>>();
+
+
+            var mockContext = new Mock<IApplicationDbContext>();
+            mockContext.SetupGet(c => c.Gigs).Returns(_mockGigs.Object);
+
+
+            _repository = new GigRepository(mockContext.Object);
+        }
+
+
+        [Fact]
+        public void GetUpcomingGigsByArtist_GigIsInThePast_ShouldNotBeReturned()
+        {
+            var gig = new Gig()
+            {
+                DateTime = DateTime.Now.AddDays(-1),
+                ArtistId = "1"
+            };
+
+            _mockGigs.SetSource(new[] { gig });
+
+            var gigs = _repository.GetUpcomingGigsByArtist("1");
+
+            gigs.Should().BeEmpty();
+        }
+
+    }
+}
