e6ef430882fb7a20018c5020fca8d671464e7d14
Lawrence LCI <lcheunglci@gmail.com>
2022-10-08 00:13:52 -0700

    Add first integration test with the web app factory

--- GigHub.IntegrationTests/UnitTest1.cs
+++ GigHub.IntegrationTests/UnitTest1.cs
@@ -1,18 +1,31 @@
-﻿namespace GigHub.IntegrationTests
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace GigHub.IntegrationTests
 {
     public class UnitTest1
     {
+        // https://learn.microsoft.com/en-us/aspnet/core/test/integration-tests?view=aspnetcore-6.0
+
         [Fact]
         public async Task HelloWorldTest()
         {
-            //var application = new WebApplicationFactory<Program>()
-            //    .WithWebHostBuilder(builder =>
-            //    {
-            //        // ... Configure test services
-            //    });
+            var application = new WebApplicationFactory<Program>()
+                .WithWebHostBuilder(builder =>
+                {
+                    // ... Configure test services
+                });
 
-            //var client = application.CreateClient();
+            var client = application.CreateClient();
             //...
+
+            // Act
+            var response = await client.GetAsync("/");
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("text/html; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+
         }
 
     }
--- GigHub/Program.cs
+++ GigHub/Program.cs
@@ -65,3 +65,5 @@ app.MapControllerRoute(
 app.MapRazorPages();
 
 app.Run();
+
+public partial class Program { }
