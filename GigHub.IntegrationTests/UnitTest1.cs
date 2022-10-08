c98f038617e3074623396b5a126a2498f6508ab4
Lawrence LCI <lcheunglci@gmail.com>
2022-10-07 23:59:21 -0700

    Add integration test dependencies

---
+++ GigHub.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿namespace GigHub.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public async Task HelloWorldTest()
+        {
+            //var application = new WebApplicationFactory<Program>()
+            //    .WithWebHostBuilder(builder =>
+            //    {
+            //        // ... Configure test services
+            //    });
+
+            //var client = application.CreateClient();
+            //...
+        }
+
+    }
+}
