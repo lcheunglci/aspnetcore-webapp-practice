3a5a6aeaeee67ce9909fae46c66d639428cb0d0c
Lawrence LCI <lcheunglci@gmail.com>
2022-10-09 00:39:28 -0700

    Add custom web application factory, a helper method to seed the userdata

---
+++ GigHub.IntegrationTests/Utilities.cs
@@ -0,0 +1,33 @@
+﻿using GigHub.Core.Models;
+using GigHub.Persistence;
+
+namespace GigHub.IntegrationTests
+{
+    public class Utilities
+    {
+        public static void InitializeDbForTests(ApplicationDbContext context)
+        {
+            if (context.Users.Any())
+                return;
+
+            context.Users.AddRange(GetSeedingUsers());
+            context.SaveChanges();
+        }
+
+        public static void ReinitializeDbForTests(ApplicationDbContext context)
+        {
+            context.Users.RemoveRange(context.Users);
+            InitializeDbForTests(context);
+        }
+
+        public static List<ApplicationUser> GetSeedingUsers()
+        {
+            return new List<ApplicationUser>()
+            {
+                new ApplicationUser { UserName = "user1", Name = "user1", Email = "-", PasswordHash = "-" },
+                new ApplicationUser { UserName = "user2", Name = "user2", Email = "-", PasswordHash = "-" }
+
+            };
+        }
+    }
+}
