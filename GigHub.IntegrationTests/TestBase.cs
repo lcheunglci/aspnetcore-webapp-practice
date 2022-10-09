f20176d5a8bbb90b32e0040f9251ece6eef97d16
Lawrence LCI <lcheunglci@gmail.com>
2022-10-09 00:41:43 -0700

    Add transaction scope in the testbase for isolated test behaviour

--- GigHub.IntegrationTests/TestBase.cs
+++ GigHub.IntegrationTests/TestBase.cs
@@ -1,20 +1,24 @@
+using System.Transactions;
+
 namespace GigHub.IntegrationTests
 {
     public class TestBase : IDisposable
     {
         // https://stackoverflow.com/questions/12976319/xunit-net-global-setup-teardown
+        // https://learn.microsoft.com/en-us/sql/ssdt/how-to-write-sql-server-unit-test-that-runs-in-single-transaction-scope?view=sql-server-ver16
+        TransactionScope _trans;
 
         protected TestBase()
         {
             // Global Initialization happens here.  Called before every test.
             // var configuration = new GigHub.Migrations
-
-
+            _trans = new TransactionScope();
         }
 
         public void Dispose()
         {
             // Global Clean up happens here.  Called after every test.
+            _trans.Dispose();
         }
     }
 }
